9051da835f5992247e5b228e550c6d1771579fb3
Mohammad Abu-Amara <abuamaramstudy@gmail.com>
2019-03-26 19:07:38 +0200

    commit Layout

---
+++ app/src/main/java/com/psut/pool/Activities/PersonalInfoActivity.java
@@ -0,0 +1,15 @@
+package com.psut.pool.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.psut.pool.R;
+
+public class PersonalInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_info);
+    }
+}
---
+++ app/src/main/java/com/psut/pool/Activities/StartActivity.java
@@ -0,0 +1,18 @@
+package com.psut.pool.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.psut.pool.R;
+
+import java.util.Objects;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+    }
+}
